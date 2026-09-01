2215031e318c91fe1edb811a2b8b2e7f8d79730c
Arthur S. Mallmann <chatiefol@gmail.com>
2026-09-01 09:55:41 -0300

    Adicionado banco de dados, porém, incompleto ainda

--- banco_de_dados/aryn_database.sql
+++ banco_de_dados/aryn_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 01/09/2026 às 14:34
+-- Tempo de geração: 31/08/2026 às 16:09
 -- Versão do servidor: 10.4.27-MariaDB
 -- Versão do PHP: 8.2.12
 
